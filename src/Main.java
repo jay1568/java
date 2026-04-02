b34de6fe62e65e08e9ed471e2053d0613b12e3b0
jay1568 <xavier15610867278@163.com>
2026-04-02 16:58:41 +0800

    第一次提交

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int age = 23;
+        long bigNum = 100000L;
+        double price = 99.5;
+        boolean isStudent = true;
+        String name = "沈星回";
+        System.out.println(age);
+        System.out.println("名字是" + name);
+    }
+}
