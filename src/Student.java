9198ede8f32448314c46687612264822d82bcfdb
jay1568 <xavier15610867278@163.com>
2026-04-08 10:10:15 +0800

    练习类的定义和方法

--- src/Student.java
+++ src/Student.java
@@ -3,23 +3,19 @@ package Day01.src;
 public class Student {
     String name;
     int age;
-    String gender;
-    int num;
+    public void study(){
+        System.out.println(name + "正在学习");
+    }
+    public void introduce(){
+        System.out.println("我是" + name +"，今年" + age +"岁");
+    }
 
     public static void main(String[] args) {
-        Student stu1 = new Student();
-        stu1.name = "张三";
-        stu1.age = 20;
-        stu1.gender = "男";
-        stu1.num = 2024001;
-        Student stu2 = new Student();
-        stu2.name = "李四";
-        stu2.age = 19;
-        stu2.gender = "女";
-        stu2.num = 2024002;
-
-        System.out.println(stu1.name+","+stu1.age+","+stu1.gender+","+stu1.num);
-        System.out.println(stu2.name+","+stu2.age+","+stu2.gender+","+stu2.num);
+        Student stu = new Student();
+        stu.name = "张三";
+        stu.age = 18;
+        stu.introduce();
+        stu.study();
     }
 
 }
