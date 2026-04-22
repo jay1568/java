b07a74a590534e766ce9954d196360d883958edf
jay1568 <xavier15610867278@163.com>
2026-04-22 19:17:04 +0800

    练习接口多实现：A、B、C

---
+++ src/April22th/A.java
@@ -0,0 +1,5 @@
+package Day01.src.April22th;
+
+public interface A {
+    void a();
+}
---
+++ src/April22th/Test.java
@@ -0,0 +1,13 @@
+package Day01.src.April22th;
+
+public class Test {
+    public static void main(String[] args) {
+        A a = new C();
+        B b = new C();
+        C c = new C();
+        a.a();
+        b.b();
+        c.a();
+        c.b();
+    }
+}
