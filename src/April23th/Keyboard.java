e4d8bbbd0f372593eee39e8fc948b8ead94faa09
jay1568 <xavier15610867278@163.com>
2026-04-23 11:46:18 +0800

    练习接口在开发中的作用：电脑接设备

---
+++ src/April23th/Keyboard.java
@@ -0,0 +1,8 @@
+package Day01.src.April23th;
+
+public class Keyboard implements Usb{
+    @Override
+    public void connect() {
+        System.out.println("键盘已连接");
+    }
+}
---
+++ src/April23th/Usb.java
@@ -0,0 +1,5 @@
+package Day01.src.April23th;
+
+public interface Usb {
+    void connect();
+}
